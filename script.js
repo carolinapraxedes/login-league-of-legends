f987d0fde1d3fd72530254dea08e073b9ffd882e
carolinapraxedes <ana_carolina47@hotmail.com>
2022-06-19 20:06:37 -0300

    login quase finalizado, faltando botão de logar e os links

---
+++ script.js
@@ -0,0 +1,48 @@
+//Início da codificação dos inputs de username e senha
+const inputs = document.querySelectorAll('.input');
+const button = document.getElementsByClassName('.loginButton');
+
+const handleFocus = ({target}) =>{
+    const span = target.previousElementSibling;
+    span.classList.add('span-active');
+
+    // eu estou pegando o irmao anterior do input que é o span
+    //Quando o span for clicado, a class span-active é acionada
+}
+
+const handleFocusOut = ({target}) =>{
+
+    if(target.value == ''){
+        // Se tiver nada no input, pode remover o span-active
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+    }
+
+    
+}
+
+//Fim da codificação dos inputs de username e senha
+
+// Início de verificação se os inputs estão com value null ou não
+// o Botão de logar vai mudar de estilização de acordo com os values de username e senha
+
+const handleChange = () => {
+    const [username, password] = inputs;
+    
+    console.log('username ='+username.value);
+    console.log('senha ='+password.value);
+    if(username.value && password.value.length >= 8){
+        console.log('passou');
+        
+        button.removeAttribute('disabled');
+    }
+   
+
+}
+
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
+
+// Fim de verificação se os inputs estão com value null ou não
